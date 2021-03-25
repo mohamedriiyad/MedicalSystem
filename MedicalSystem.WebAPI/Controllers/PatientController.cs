c6c9f0af5d5c8f71eda0233f61eaa1a90ac789db
3mrEmad <3mr3mad47@gmail.com>
2021-03-25 03:19:09 +0200

    amredit

---
+++ MedicalSystem.WebAPI/Controllers/PatientController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalSystem.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+
+        public string Get()
+        {
+            return "this is patient controoler";
+        }
+
+
+    }
+}
